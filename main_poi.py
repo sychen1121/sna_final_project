8db77369a4d1f1348f653fef50616ab639fcc9de
YIWEI-CHEN <feberium@gmail.com>
2015-01-05 23:44:12 +0800

    main_poi modification

--- main_poi.py
+++ main_poi.py
@@ -9,4 +9,4 @@ poi.update_user_hometown(social_graph, poi_graph)
 # poi.update_hometown_geocode(social_graph, user_list)
 user_near_places = poi.spot_candidate2(social_graph, poi_graph, user_list, place_list,24)
 
-#pr.run_method(cf_user_mp)
+pr.run_method(cf_user_mp)
