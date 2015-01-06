ed2cb12678d3c7b8e442e116dc7b921b6f4060d0
YIWEI-CHEN <feberium@gmail.com>
2015-01-06 22:51:09 +0800

    modification for cf

--- main_poi.py
+++ main_poi.py
@@ -8,6 +8,6 @@ social_graph = poi.create_social_graph(file_path)
 poi_graph, user_list, place_list = poi.create_poi_graph_from_file(file_path)
 poi.update_user_hometown(social_graph, poi_graph)
 # poi.update_hometown_geocode(social_graph, user_list)
-user_near_places = poi.spot_candidate2(social_graph, poi_graph, user_list, place_list,nprocs)
+#user_near_places = poi.spot_candidate2(social_graph, poi_graph, user_list, place_list,nprocs)
 
-#pr.run_method(pr.cf_user_mp)
+pr.run_method(pr.cf_user_mp)
