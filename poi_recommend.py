7292e304d8576c465cbf2ca3bec54b842d5a8137
YIWEI-CHEN <feberium@gmail.com>
2015-01-07 01:19:41 +0800

    fix bugs of cf_user

--- poi_recommend.py
+++ poi_recommend.py
@@ -245,7 +245,7 @@ def cf_user(top_k=10, output_path='../output/poi_recommendation'):
 
 # =============== cf multiprocess ==================
 
-def cf_user_mp(top_k=10, output_path='../output/poi_recommendation/', nprocs = 16):
+def cf_user_mp(top_k=10, output_path='../output/poi_recommendation/', nprocs = 10):
     s= time()
     predict_dict = dict()
     users_unvisited_place_score = dict()
